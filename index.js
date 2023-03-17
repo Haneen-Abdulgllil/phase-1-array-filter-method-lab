5f17c77a931b69aed26b7f531dc2f65d65f20cc9
Haneen-Abdulgllil <haneenabdulglil2018@yahoo.com>
2023-03-17 10:49:59 +0300

    assignment complates

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, condition){
+    return drivers.filter(e => e.toLowerCase() === condition.toLowerCase());
+}
+
+function fuzzyMatch(drivers, condition){
+    return drivers.filter(e => e.startsWith(condition));
+}
+
+function matchName(drivers, condition){
+    return drivers.filter(e => e.name === condition);
+}
